aa1d718739d7317ee430bfbea029dbaaa3ab8740
kumari-83 <jpuja6664@gmail.com>
2024-06-15 21:33:16 +0530

    Added js file

---
+++ js/music-list.js
@@ -0,0 +1,125 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Bhole Charan - Aradhna",
+    artist: "Muktidaan",
+    img: "bhole charan",
+    src: "bhole charan"
+  },
+  {
+    name: "Mere Ghar Raam",
+    artist: "Jubin Nautiyal",
+    img: "mere ghar",
+    src: "mere ghar"
+  },
+  {
+    name: "Rudrastakam",
+    artist: "Unkwon Source",
+    img: "namami shamisham",
+    src: "namami shamisham"
+  },
+  {
+    name: "Parvati Vallabhastakam",
+    artist: "Madhavi Jha",
+    img: "parvati",
+    src: "parvati"
+  },
+  {
+    name: "Mann Bsiya",
+    artist: "Alka Yaganik",
+    img: "pawan phrabhati",
+    src: "pawan phrabhati"
+  },
+  {
+    name: "Savaiyaa",
+    artist: "Shreya Ghosal",
+    img: "radhe krishna ki",
+    src: "radhe krishna ki"
+  },
+  {
+    name: "Ram Doot Hanumaan",
+    artist: "Nikhar Juneja",
+    img: "ram doot",
+    src: "ram doot"
+  },
+  {
+    name: "Janak Nandani",
+    artist: "Saloni Thakkar",
+    img: "ram ko dekh",
+    src: "ram ko dekh"
+  },
+  {
+    name: "Ram Lalaa",
+    artist: "Vishal Mishra",
+    img: "ram lala",
+    src: "ram lala"
+  },
+  {
+    name: "Rama Rama Ratte",
+    artist: "Sadhoband",
+    img: "rama rama",
+    src: "rama rama"
+  },
+  {
+    name: "Shree Hari Stotram",
+    artist: "Unknown Source",
+    img: "shree hari",
+    src: "shree hari"
+  },
+  {
+    name: "Shree Krishna Govind",
+    artist: "Jubin Nautiyal",
+    img: "shree krishna",
+    src: "shree krishna"
+  },
+  {
+    name: "Ye Chamak Ye Damak 2.0",
+    artist: "P.Shudhir ji Vyas",
+    img: "ye chamak",
+    src: "ye chamak"
+  },
+  {
+    name: "Hanuman Chalisha",
+    artist: "Rasraj ji Maharaj",
+    img: "hanuman chalisa",
+    src: "hanuman chalisa"
+  },
+  {
+    name: "Keejo Kesari Ke Laal",
+    artist: "Karan Kahar",
+    img: "keejo kesri ke laal",
+    src: "keejo kesri ke laal"
+  },
+  {
+    name: "Mangal Bhawan",
+    artist: "Sadhoband",
+    img: "mangal bhawan",
+    src: "mangal bhawan"
+  },
+  {
+    name: "Mahakal ki Gulami",
+    artist: "Kisan Bhagat",
+    img: "mahakal ki gulami",
+    src: "mahakal ki gulami"
+  },
+
+
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
